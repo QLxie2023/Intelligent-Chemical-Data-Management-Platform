65336b45cee21ffac67a51cadb9e8c67d3b3c6bc
QLxie2023 <148576271+QLxie2023@users.noreply.github.com>
2026-04-14 20:47:15 +0800

    Basic function

--- Desktop/code/backend/src/main/java/chem_data_platform/demo/repository/ChemicalInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package chem_data_platform.demo.repository;
-
-import chem_data_platform.demo.entity.ChemicalInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ChemicalInfoRepository extends JpaRepository<ChemicalInfo, Long> {
-}
--- Desktop/code/backend/src/main/java/chem_data_platform/demo/repository/FileInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package chem_data_platform.demo.repository;
-
-import chem_data_platform.demo.entity.FileInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FileInfoRepository extends JpaRepository<FileInfo, Long> {
-}
